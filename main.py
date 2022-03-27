58a05c4ba6b25cd62f3eaaff0e87dfdade94ea4b
Karolina Krzęcio <karolina.krzecio@gmail.com>
2022-03-27 19:12:57 +0200

    Replace word keys main.py

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ shopping_dict = {
 
 print("Lista zakupów")
 
-for keys, values in shopping_dict.items():
-      print(f"Idę do {keys.title()}, kupuję tu następujące rzeczy: {str(values).title()}")
+for shop, values in shopping_dict.items():
+      print(f"Idę do {shop.title()}, kupuję tu następujące rzeczy: {str(values).title()}")
       len_values1 = len(shopping_dict["piekarnia"])
       len_values2 = len(shopping_dict["warzywniak"])  
 print (f"W sumie kupuję {len_values1 + len_values2} produktów.")
